50eb124cefa8dfe61430ba54ef63591579995cf5
mowern <mower.n@northeastern.edu>
2024-02-25 19:17:19 -0500

    player aiming/shooting anims

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -7,38 +5,71 @@ public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject bullet;
-    private Vector3 mousePos;
-    private Vector3 aimPos;
-    private Quaternion rot;
+    [SerializeField] private GameObject shootEffect;
+    private Transform playerTransform;
+    private Vector2 aimDirection;
     private float cooldown;
+    private bool isSpriteFlipped = false;
 
     private float projectileSpeed = 7.0f;
 
     void Start()
     {
-        
+        playerTransform = GameObject.FindGameObjectWithTag("Player")
+                                        .GetComponent<Transform>();
     }
 
     void Update()
     {
+        Aim();
         cooldown -= Time.deltaTime;
         if (Input.GetButton("Fire1") && cooldown <= 0)
         {
-            shootGun();
+            Shoot();
             cooldown = 0.2f;
         }
     }
-    private void shootGun()
+    
+    private void Aim()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f;
+        aimDirection = (mousePosition - transform.position).normalized;
 
-        GameObject projectile = Instantiate(bullet, transform.position, quaternion.identity);
+        float armAngle;
+        if (mousePosition.x < transform.position.x)
+        {
+            if (isSpriteFlipped) FlipSprite(); // if facing right, face left
+            armAngle = Vector2.SignedAngle(Vector2.left, aimDirection);
+        }
+        else
+        {
+            if (!isSpriteFlipped) FlipSprite(); // if facing left, face right
+            armAngle = Vector2.SignedAngle(Vector2.right, aimDirection);
+        }
+        transform.rotation = Quaternion.Euler(0, 0, armAngle);
+    }
+
+    private void FlipSprite()
+    {
+        Vector3 newScale = new(-playerTransform.localScale.x, 
+                            playerTransform.localScale.y, 
+                            playerTransform.localScale.z);
+        playerTransform.localScale = newScale;
+        isSpriteFlipped = !isSpriteFlipped;
+    }
+    
+    private void Shoot()
+    {
+        Vector3 bulletPosition = new(transform.position.x + aimDirection.x, 
+                                    transform.position.y + aimDirection.y, 
+                                    transform.position.z);
+        GameObject projectile = Instantiate(bullet, bulletPosition, quaternion.identity);
         Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
         if(projectileRb != null)
         {
-            Vector2 shootDirection = (mousePosition - transform.position).normalized;
-            projectileRb.velocity = new Vector2(shootDirection.x, shootDirection.y) * projectileSpeed;
+            projectileRb.velocity = new Vector2(aimDirection.x, aimDirection.y) * projectileSpeed;
         }
+        Instantiate(shootEffect, transform.position, transform.rotation);
     }
 }
