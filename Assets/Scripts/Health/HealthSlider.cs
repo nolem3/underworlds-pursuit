d8cf1ae3737bcdd32f0a30a64ab70a7f906ad589
unknown <lemerynolan@gmail.com>
2024-02-26 04:04:04 -0500

    Player death and Level 1 Modification
    - Level restart on player death
    - Bigger starting platform on Level 1

--- Assets/Scripts/Health/HealthSlider.cs
+++ Assets/Scripts/Health/HealthSlider.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class HealthSlider : AHealthTracker
 {
@@ -10,6 +11,13 @@ public class HealthSlider : AHealthTracker
     public override void HealthChanged(AHealth healthScript)
     {
         healthSlider.maxValue = healthScript.GetMaxHealth(); 
-        healthSlider.value = healthScript.GetHealth(); 
+        healthSlider.value = healthScript.GetHealth();
+
+        // Check for player death:
+        if (healthSlider.value <= 0 && gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Player dead");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        } 
     }
 }
